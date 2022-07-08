b1edfe235f1339062cc293e4f2e3a3fabec9b9d0
Lumina <erik04.nivala22@gmail.com>
2022-07-08 09:25:33 -0500

    Small clean up

--- Hangman2.0/src/main/java/Hangman/Main.java
+++ Hangman2.0/src/main/java/Hangman/Main.java
@@ -1,9 +1,8 @@
 package Hangman;
 
 public class Main {
-
     public static void main(String[] args) {
-        GameLoop game = new GameLoop();
-        game.startGame();
+       GameLoop game = new GameLoop();
+       game.startGame();
     }
 }
